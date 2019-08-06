b5f1aac179d37a5f20264e3cc478eddc864fd8ce
Arash Fayyazi <fayyazi@usc.edu>
2019-08-06 11:06:33 -0700

    SFQLib_v5.1

--- SFQLib/SFQ_library.sv
+++ SFQLib/SFQ_library.sv
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////
-//////// Ramy Tadros 08/15/2018  ////////////
-///////  HCLC allowed modules in SV - Version-5
+// Ramy Tadros & Arash Fayyazi 8/06/2019 //
+///////  HCLC allowed modules in SV - Version-5.1
 /////////////////////////////////////////////////
 
 /*the following is the list of macros that need to be defined in the topmodule for testing (no SDF)
@@ -67,6 +67,66 @@ module SFQsplit (SFQ in, out1, out2);
 endmodule // split 
 ///////////////////////////////////////////////////
 
+///////////////////////////////////////////////////
+module SFQor2 (SFQ clkin, in1, in2, out);
+	
+	SFQpropagationDelay gPD ();
+	
+	always begin
+		clkin.receive();
+		if (in1.isReceived(1'b1) | in2.isReceived(1'b1)) begin
+			gPD.tPD();
+			out.send();
+		end
+	end
+	
+	SFQtimingcheck2 TC (clkin.data, in1.data, in2.data);
+endmodule //or2
+///////////////////////////////////////////////////
 
+///////////////////////////////////////////////////
+module SFQor2 (SFQ clkin, in1, in2, out);
+	
+	SFQpropagationDelay gPD ();
+	
+	always begin
+		clkin.receive();
+		if (in1.isReceived(1'b1) | in2.isReceived(1'b1)) begin
+			gPD.tPD();
+			out.send();
+		end
+	end
+	
+	SFQtimingcheck2 TC (clkin.data, in1.data, in2.data);
+endmodule //or2
+///////////////////////////////////////////////////
 
+///////////////////////////////////////////////////
+module SFQinv1 (SFQ clkin, in, out);
 
+	SFQpropagationDelay gPD ();
+
+	always begin
+		clkin.receive();
+		if (!(in.isReceived(1'b1))) begin
+			gPD.tPD();
+			out.send();
+		end
+	end
+	SFQtimingcheck1 TC (clkin.data, in.data);
+endmodule //inv1
+///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
+module SFQjtl (SFQ in, out);
+	
+	SFQpropagationDelay gPD ();
+	
+	always begin
+		in.receive();
+		gPD.tPD();
+		out.send();
+	end
+		
+endmodule //jtl
+///////////////////////////////////////////////////
